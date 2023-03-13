7829656d6ab780b10aad20e3f7d2acfa8fab9dab
Arditti93 <alex.arditti@hotmail.com>
2023-03-13 15:10:23 +0000

    added addUser notes

--- src/components/addUser/AddUser.js
+++ src/components/addUser/AddUser.js
@@ -1,26 +1,33 @@
 import { useState } from "react";
+// import useState
 import "./AddUser.css";
+// import css file
 
 import { addUser } from "../../utils";
+// import addUser from utils
 
 const AddUser = ({ user, setUser }) => {
+  // Props are passed down from the parent userWrapper componet
   const [username, setUsername] = useState();
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
+  // define our state hooks to store username, email and password values from our form
 
+  // This function is called when our below form has been submitted
   const onRegisterSubmit = async (e) => {
     e.preventDefault();// prevent the page from reloading
     console.log(username, email, password);
-    // call addUser function from our utils folder
-    
+    // call addUser function from our utils folder, pass the state variables as aurguments into the function
     await addUser(username, email, password);
   };
 
   return (
     <div className="adduser-wrapper">
+      {/* onSubmit will call the onRegisterSubmit function once the button in the form is clicked  */}
       <form className="add-user-form" onSubmit={onRegisterSubmit}>
         <label>
           Username:
+          {/* onChange event will up date the state hook everytime something is typed into the input box */}
           <input
             placeholder="username"
             onChange={(e) => setUsername(e.target.value)}
@@ -28,6 +35,7 @@ const AddUser = ({ user, setUser }) => {
         </label>
         <label>
           Email:
+          {/* onChange event will up date the state hook everytime something is typed into the input box */}
           <input
             placeholder="email"
             onChange={(e) => setEmail(e.target.value)}
@@ -35,11 +43,13 @@ const AddUser = ({ user, setUser }) => {
         </label>
         <label>
           Password:
+          {/* onChange event will up date the state hook everytime something is typed into the input box */}
           <input
             placeholder="password"
             onChange={(e) => setPassword(e.target.value)}
           />
         </label>
+        {/* button with a type of submit, this run the onSubmit event on the form */}
         <button type="submit">Register</button>
       </form>
     </div>
